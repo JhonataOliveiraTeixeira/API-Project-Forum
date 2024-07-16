018b46670239b348a3878d78dbdb3852144bd63f
JhonataOliveiraTeixeira <jonasdeot@gmail.com>
2024-07-16 18:48:30 -0300

    Criação do caso de uso d get question by slug

--- src/domain/forum/application/test/repositories/in-memory-question-repository.ts
+++ src/domain/forum/application/test/repositories/in-memory-question-repository.ts
@@ -3,6 +3,16 @@ import { QuestionsRepository } from "../../repositories/questions-repository";
 
 export class InMemoryQuestionRepository implements QuestionsRepository{
     public items: Question[] = []
+    
+    async findBySlug(slug: string) {
+        const question = this.items.find((item)=>item.slug.value === slug)
+
+        if(!question){
+            throw new Error('Question not found')
+        }
+
+        return question
+    }
 
     async create(question: Question) {
         this.items.push(question)
