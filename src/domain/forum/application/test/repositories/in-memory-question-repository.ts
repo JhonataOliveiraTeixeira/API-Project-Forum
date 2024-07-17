ebcee1bf999f4844e36963f29481fdfe1ca5096e
JhonataOliveiraTeixeira <jonasdeot@gmail.com>
2024-07-17 18:14:54 -0300

    caso de uso de editar pergunta

--- src/domain/forum/application/test/repositories/in-memory-question-repository.ts
+++ src/domain/forum/application/test/repositories/in-memory-question-repository.ts
@@ -4,16 +4,16 @@ import { QuestionsRepository } from "../../repositories/questions-repository";
 export class InMemoryQuestionRepository implements QuestionsRepository{
     public items: Question[] = []
     
-        async findById(id: string) {
-    
-            const question = this.items.find((item)=> item.id.toString()  === id)
+    async findById(id: string) {
         
-            if(!question){
-                throw new Error('Question not found')
-            }
-            
-            return question
+        const question = this.items.find((item)=> item.id.toString()  === id)
+        
+        if(!question){
+            throw new Error('Question not found')
         }
+        
+        return question
+    }
     
     async findBySlug(slug: string) {
         const question = this.items.find((item)=>item.slug.value === slug)
@@ -28,11 +28,16 @@ export class InMemoryQuestionRepository implements QuestionsRepository{
     async create(question: Question) {
         this.items.push(question)
     }
-
+    
     async delete(question: Question){
         const itemIndex = this.items.findIndex(item => item.id === question.id)
-
+        
         this.items.splice(itemIndex, 1)
     }
-
+    
+    async save(question: Question){
+        const itemIndex = this.items.findIndex(item => item.id === question.id)
+        
+        this.items[itemIndex] = question
+    }
 }
---
+++ src/domain/forum/application/test/testes/edit-question.spec.ts
@@ -0,0 +1,65 @@
+import { expect, describe, beforeEach, it } from 'vitest'
+import { InMemoryQuestionRepository } from '../repositories/in-memory-question-repository'
+import { makeQuestion } from '../factories/make-question'
+import { EditQuestionUseCase } from '../../use-cases/edit-question/edit-question'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+let inMemoryQuestionRepository: InMemoryQuestionRepository
+let sut : EditQuestionUseCase
+
+describe('Edit Question',()=>{
+
+    beforeEach(()=>{
+
+        inMemoryQuestionRepository = new InMemoryQuestionRepository()
+        sut = new EditQuestionUseCase(inMemoryQuestionRepository)
+
+    })
+
+    it('should be able to delte a question', async () => {
+
+        const newQuestion = makeQuestion({
+            authorId: new UniqueEntityID('1')
+        },
+            new UniqueEntityID('question-1')
+        )
+        inMemoryQuestionRepository.create(newQuestion)
+      
+        await sut.execute({
+            questionId: 'question-1',
+            authorId: '1',
+            content: 'Nova pergunta',
+            title: 'Novo titulo'
+        })
+      
+        expect(inMemoryQuestionRepository.items[0]).toMatchObject({
+            content: 'Nova pergunta',
+            title: 'Novo titulo'
+        })
+
+      })
+
+      it('should be not able to edit a question from another user', async () => {
+
+        const newQuestion = makeQuestion({
+            authorId: new UniqueEntityID('1')
+        },
+            new UniqueEntityID('question-1')
+        )
+        inMemoryQuestionRepository.create(newQuestion)
+      
+        
+      
+        expect(()=>{
+            return sut.execute({
+                questionId: 'question-1',
+                authorId: '2',
+                content: 'Nova pergunta',
+                title: 'Novo titulo'
+            })
+        }).rejects.toBeInstanceOf(Error)
+
+      })
+
+} )
+
